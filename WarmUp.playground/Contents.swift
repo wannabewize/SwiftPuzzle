e3eee41e5755e937a68319336289475b9dfb7c17
wannabewize <wannabewize@gmail.com>
2018-01-12 14:46:00 +0900

    오타 수정
    result -> result1

--- WarmUp.playground/Contents.swift
+++ WarmUp.playground/Contents.swift
@@ -3,8 +3,8 @@ func add(_ i: Int, _ j: Int) -> Int {
     return 0
 }
 
-let result = add(1, 2)
-assert(3 == result, "1 + 2 should be 3, but \(result1)")
+let result1 = add(1, 2)
+assert(3 == result1, "1 + 2 should be 3, but \(result1)")
 
 let result2 = add(3, 4)
 assert(7 == result2, "3 + 4 should be 7, but \(result2)" )
